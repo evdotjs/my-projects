edbfd63e407f5ce6abb6e10151d444092c4faf8c
Ev McNeal <evmcneal@icloud.com>
2020-02-23 21:11:00 -0800

    add [delete] buttons after each to-do item

--- todo-list/todos.js
+++ todo-list/todos.js
@@ -1,60 +1,38 @@
+//create and manipulate an array of to-dos
 let todoList = {
   todos: [],
-  displayTodos: function() {
-    if(this.todos.length === 0) {
-      console.log('Your todo list is empty!')
-    } else {
-      console.log('My Todos:');
-      for (let i = 0; i < this.todos.length; i++) {
-        if (this.todos[i].completed) {
-          console.log(`[x] ${this.todos[i].todoText}`);
-        } else {
-          console.log(`[ ] ${this.todos[i].todoText}`);
-        }
-      }
-    }
-    return this.todos;
-  },
   addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    return this.displayTodos();
   },
   changeTodo: function(position, todoText) {
     this.todos[position].todoText = todoText;
-    return this.displayTodos();
   },
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
-    return this.displayTodos();
   },
   toggleCompleted: function(position) {
     let todo = this.todos[position];
     todo.completed = !todo.completed;
-    return this.displayTodos();
   },
   toggleAll: function() {
     let totalTodos = this.todos.length;
     let completedTodos = this.todos.filter(todo => todo.completed === true).length;
-
-    //if everything is true, make everything false
-    if (completedTodos === totalTodos) {
-      this.todos.forEach( todo => todo.completed = false );
-    } else {
+    completedTodos === totalTodos ?
+      this.todos.forEach( todo => todo.completed = false ) :
       this.todos.forEach( todo => todo.completed = true );
-    }
-    return this.displayTodos();
   }
 }
 
+//handle user interations
 const handlers = {
-  displayTodos: () => { todoList.displayTodos(); },
   addTodo: () => {
     const addTodoInput = document.querySelector('.addTodoInput');
     todoList.addTodo(addTodoInput.value);
     addTodoInput.value = '';
+    view.displayTodos();
   },
   changeTodo: () => {
     const changeTodoPosition = document.querySelector('.changeTodoPosition');
@@ -63,16 +41,54 @@ const handlers = {
     todoList.changeTodo(changeTodoPosition.valueAsNumber-1, changeTodoText.value);
     changeTodoPosition.value = '';
     changeTodoText.value = '';
+    view.displayTodos();
   },
-  deleteTodo: () => {
-    const deleteTodoPosition = document.querySelector('.deleteTodoPosition');
-    todoList.deleteTodo(deleteTodoPosition.valueAsNumber-1);
-    deleteTodoPosition.value = '';
+  deleteTodo: (position) => {
+    todoList.deleteTodo(position);
+    view.displayTodos();
   },
   toggleCompleted: () => {
     const toggleCompletedPosition = document.querySelector('.toggleCompletedPosition');
     todoList.toggleCompleted(toggleCompletedPosition.valueAsNumber-1);
     toggleCompletedPosition.value = '';
+    view.displayTodos();
   },
-  toggleAll: () => { todoList.toggleAll(); }
+  toggleAll: () => {
+    todoList.toggleAll();
+    view.displayTodos();
+  }
 };
+
+//show the to-do list in the DOM
+const view = {
+  displayTodos: function() {
+    const toDoUL = document.querySelector('.toDoUL');
+    toDoUL.innerHTML = "";
+    todoList.todos.forEach((todo, index) => {
+      let toDoLi = document.createElement('li');
+      toDoLi.id = index;
+      todo.completed ?
+        toDoLi.textContent = `[x] ${todo.todoText}`:
+        toDoLi.textContent = `[ ] ${todo.todoText}`;
+      toDoUL.appendChild(toDoLi);
+      toDoLi.appendChild(this.createDeleteButton());
+    });
+  },
+  createDeleteButton: function() {
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteButton'
+    return deleteButton;
+  },
+  setUpEventListeners: function() {
+    const toDoUL = document.querySelector('.toDoUL');
+    toDoUL.addEventListener('click', function(event){
+      let elementClicked = event.target;
+      if (elementClicked.className === 'deleteButton') {
+        handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+      }
+    });
+  }
+}
+
+view.setUpEventListeners();
