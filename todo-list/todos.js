96cbc727a4c6d8481a1500e98b943cf719f149ba
Ev McNeal <evmcneal@icloud.com>
2020-02-26 22:20:00 -0800

    add vue.js and refactor todos.js

--- todo-list/todos.js
+++ todo-list/todos.js
@@ -1,104 +1,45 @@
-//create and manipulate an array of to-dos
-let todoList = {
-  todos: [],
-  addTodo: function(todoText) {
-    this.todos.push({
-      todoText: todoText,
-      completed: false
-    });
+const toDoList = [
+  {
+         text: "first",
+    completed: false,
+        class: 'incomplete'
   },
-  changeTodo: function(position, todoText) {
-    this.todos[position].todoText = todoText;
+  {
+         text: "second",
+    completed: false,
+        class: 'incomplete'
   },
-  deleteTodo: function(position) {
-    this.todos.splice(position, 1);
+  {
+         text: "third",
+    completed: false,
+        class: 'incomplete'
   },
-  toggleCompleted: function(position) {
-    let todo = this.todos[position];
-    todo.completed = !todo.completed;
-  },
-  toggleAll: function() {
-    let totalTodos = this.todos.length;
-    let completedTodos = this.todos.filter(todo => todo.completed === true).length;
-    completedTodos === totalTodos ?
-      this.todos.forEach( todo => todo.completed = false ) :
-      this.todos.forEach( todo => todo.completed = true );
-  }
-}
+];
 
-//handle user interations
-const handlers = {
-  addTodo: () => {
-    const addTodoInput = document.querySelector('.addTodoInput');
-    todoList.addTodo(addTodoInput.value);
-    addTodoInput.value = '';
-    view.displayTodos();
+new Vue({
+  el: 'main',
+  data: {
+         title: 'To-Do List',
+         todos: toDoList,
+         newToDo: ""
   },
-  changeTodo: () => {
-    const changeTodoPosition = document.querySelector('.changeTodoPosition');
-    const changeTodoText = document.querySelector('.changeTodoText');
-
-    todoList.changeTodo(changeTodoPosition.valueAsNumber-1, changeTodoText.value);
-    changeTodoPosition.value = '';
-    changeTodoText.value = '';
-    view.displayTodos();
-  },
-  deleteTodo: (position) => {
-    todoList.deleteTodo(position);
-    view.displayTodos();
-  },
-  toggleCompleted: (position) => {
-    todoList.toggleCompleted(position);
-    view.displayTodos();
-  },
-  toggleAll: () => {
-    todoList.toggleAll();
-    view.displayTodos();
-  }
-};
-
-//show the to-do list in the DOM
-const view = {
-  displayTodos: function() {
-    const toDoUL = document.querySelector('.toDoUL');
-    toDoUL.innerHTML = "";
-    todoList.todos.forEach((todo, index) => {
-      let toDoLi = document.createElement('li');
-      let buttonGroup = document.createElement('div');
-      buttonGroup.className = 'btn-group';
-      toDoLi.id = index;
-      todo.completed ?
-        toDoLi.textContent = `[x] ${todo.todoText}`:
-        toDoLi.textContent = `[ ] ${todo.todoText}`;
-      toDoUL.appendChild(toDoLi);
-      toDoLi.appendChild(buttonGroup);
-      buttonGroup.appendChild(this.createToggleButton());
-      buttonGroup.appendChild(this.createDeleteButton());
-    });
-  },
-  createToggleButton: function() {
-    let toggleButton = document.createElement('button');
-    toggleButton.textContent = 'Complete';
-    toggleButton.className = 'toggleButton btn btn-outline-primary btn-sm';
-    return toggleButton;
-  },
-  createDeleteButton: function() {
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.className = 'deleteButton btn btn-outline-primary btn-sm';
-    return deleteButton;
-  },
-  setUpEventListeners: function() {
-    const toDoUL = document.querySelector('.toDoUL');
-    toDoUL.addEventListener('click', function(event){
-      let elementClicked = event.target;
-      if (elementClicked.className.startsWith('deleteButton')) {
-        handlers.deleteTodo(parseInt(elementClicked.parentNode.parentNode.id));
-      } else if (elementClicked.className.startsWith('toggleButton')) {
-        handlers.toggleCompleted(parseInt(elementClicked.parentNode.parentNode.id));
+  methods: {
+    addToDo: function() {
+      if (this.newToDo) {
+        toDoList.push({
+               text: this.newToDo,
+          completed: false,
+              class: 'incomplete'
+        });
+        this.newToDo = "";
       }
-    });
+    },
+    toggleCompleted: function(index) {
+      let todo = this.todos[index];
+      todo.completed = !todo.completed;
+      todo.completed ?
+        todo.class = 'complete':
+        todo.class = 'incomplete';
+    }
   }
-}
-
-view.setUpEventListeners();
+});
